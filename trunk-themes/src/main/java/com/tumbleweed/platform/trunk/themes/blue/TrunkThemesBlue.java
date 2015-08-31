43ac39ea24b7b311423b9e131e0ec5c0dbf84f98
wjp <wangjp@mittop.com>
2015-08-31 17:05:27 +0800

    添加 extjs框架

---
+++ trunk-themes/src/main/java/com/tumbleweed/platform/trunk/themes/blue/TrunkThemesBlue.java
@@ -0,0 +1,10 @@
+package com.tumbleweed.platform.trunk.themes.blue;
+
+/**
+ * Created by mylover on 8/31/15.
+ */
+public class TrunkThemesBlue {
+
+    public static final String THEME_NAME = "blue";
+
+}
---
+++ trunk-themes/src/main/java/com/tumbleweed/platform/trunk/themes/light/TrunkThemesLight.java
@@ -0,0 +1,10 @@
+package com.tumbleweed.platform.trunk.themes.light;
+
+/**
+ * Created by mylover on 8/31/15.
+ */
+public class TrunkThemesLight {
+
+    public static final String THEME_NAME = "light";
+
+}
